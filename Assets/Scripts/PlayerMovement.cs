fc191cf8eb727e2b95667e7b4aaa24e73442cc37
FyreFoxx <aidanshim0204@gmail.com>
2022-06-20 20:47:52 -0700

    Gotem back
    Yeah got the terrain and the player back again

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,7 +8,7 @@ public class PlayerMovement : MonoBehaviour
     public float gravity = -9.81f;
     public float jumpHeight = 2.5f;
     public Transform groundCheck;
-    public float groundRadius = 0.5f;
+    public float groundRadius = 0.75f;
     public LayerMask groundMask;
     bool isGrounded;
 
